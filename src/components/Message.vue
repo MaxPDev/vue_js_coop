eb0fa0b79f1c54a53f29d467139140e323d5a8af
Maxime Piscaglia <maximepiscaglia@hotmail.com>
2022-02-17 16:32:05 +0100

    click on pseudo in conversation show member's profile

--- src/components/Message.vue
+++ src/components/Message.vue
@@ -1,71 +1,69 @@
 <template>
-
-    <div class="card">
+  <div class="card">
     <div class="card-content">
-    <div class="media">
+      <div class="media">
         <div class="media-left">
-        <figure class="image is-48x48">
-            <img :src="avatar(member)">
-        </figure>
+          <figure class="image is-48x48">
+            <img :src="avatar(member)" />
+          </figure>
         </div>
-        <div class="media-content">
-        <p class="title is-4"> {{ member.fullname }}</p>
-        <p class="subtitle is-6"> {{ member.email }}</p>
-        </div>
-    </div>
+        <router-link :to="{name : 'Member', params :{idMember: member.id}}">
+          <div class="media-content">
+            <p class="title is-4">{{ member.fullname }}</p>
+            <p class="subtitle is-6">{{ member.email }}</p>
+          </div>
+        </router-link>
+      </div>
 
-    <div class="content">
-        {{ message.message }} 
+      <div class="content">
+        {{ message.message }}
         <!--//todo: mettre des tags ? -->
         <!-- <a>@bulmaio</a>. -->
         <!-- <a href="#">#css</a> <a href="#">#responsive</a> -->
-        <br>
-        <time>{{ message.created_at}}</time>
+        <br />
+        <time>{{ message.created_at }}</time>
+      </div>
     </div>
-    </div>
-    </div>
-
+  </div>
 </template>
 <script>
-
 import { mapGetters } from "vuex";
 
 export default {
-    // name: 'Message',
-    props: ['message'],
+  // name: 'Message',
+  props: ["message"],
 
-    data() {
-        return {
-            member: false
-        }
+  data() {
+    return {
+      member: false,
+    };
+  },
+
+  computed: {
+    ...mapGetters(["getMember"]),
+
+    dateMessage() {
+      let date = new Date(this.message.created_at);
+      return (
+        "le " +
+        date.toLocaleDateString("fr-FR") +
+        " à " +
+        date.toLocaleTimeString("fr-FR")
+      );
     },
+    // member() {
+    //     //TODO user mutation
+    //     // console.log(this.message);
 
-    computed: {
-        ...mapGetters(["getMember"]),
+    //     return this.$store.getters.getMembre(this.message.member_id);
+    // }
+  },
 
-        dateMessage() {
-            let date = new Date(this.message.created_at);
-            return (
-                "le " + date.toLocaleDateString("fr-FR") +
-                " à " +
-                date.toLocaleTimeString("fr-FR")
-            );
-        },
-        // member() {
-        //     //TODO user mutation
-        //     // console.log(this.message);
-            
-        //     return this.$store.getters.getMembre(this.message.member_id);
-        // }
-    },
-
-    mounted() {
-        // this.member = this.getMember(this.$route.params.idMember);
-        this.member = this.getMember(this.message.member_id);
-        console.war("member");
-        console.log(this.member);
-    }
-
-}
-
+  mounted() {
+    // this.member = this.getMember(this.$route.params.idMember);
+    this.member = this.getMember(this.message.member_id);
+    console.war("member");
+    console.log(this.member);
+  },
+};
 </script>
